32f751eaf1d78a2b4f0f1c1d62f8aafa2434c93c
CodeAndCode <angelo.biscola@gmail.com>
2016-06-17 11:21:06 -0300

    Update Connection.php

--- Model/Connection.php
+++ Model/Connection.php
@@ -13,15 +13,17 @@ class Connection
 
     private function getDb(array $config)
     {
-        $db = "mysql:host=".$config['host'].";dbname=".$config['dbname'];
+        $db = "mysql:host=".$config['host'].";dbname=".$config['dbname'].";charset=utf8";
         $options = array(
             \PDO::ATTR_PERSISTENT    => true,
-            \PDO::ATTR_ERRMODE       => \PDO::ERRMODE_EXCEPTION
+            \PDO::ATTR_ERRMODE       => \PDO::ERRMODE_EXCEPTION,
+            \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8'
         );
 
         // Create a new PDO instanace
         try{
             $this->db = new \PDO($db, $config['username'], $config['password'], $options);
+            $this->db->exec("SET CHARACTER SET utf8");
         }
             // Catch any errors
         catch(\PDOException $e){
