587be50c7962aa0bd3a5a576d5af879b5c58e0aa
Angelo Biscola <angelo-tec@hotmail.com>
2016-06-21 17:09:06 -0300

    Update Action.php

--- Controller/Action.php
+++ Controller/Action.php
@@ -5,33 +5,48 @@ use Cac\TwigHelper\TwigFunction;
 
 class Action {
 
-    protected $view;
-    protected $action;
-    protected $srcView;
-
     private $folder;
     private $twig;
-
-    public function __construct()
-    {
-        $this->view = new \stdClass();
-        $this->setFolder(config('app.layout.folder'));
-    }
+    private $vars;
 
     private function setFolder($folder)
     {
         $this->folder = '../'.$folder;
         $this->twig   = new \Twig_Loader_Filesystem($this->folder);
+    }
 
+    private function twigAddGlobalVars()
+    {
+        if(!empty($this->vars))
+        {
+            foreach ($this->vars as $key=>$var)
+            {
+                $this->twig->addGlobal($key, $var);
+            }
+        }
+    }
+
+    private function twigAddExtension()
+    {
+        $this->twig->addExtension(new TwigFunction());
     }
 
     private function init($reload)
     {
+        $this->setFolder(config('app.layout.folder'));
+
         $this->twig   = new \Twig_Environment($this->twig,
             ['cache'       => '../../../storage/compilation_cache',
              'auto_reload' => $reload]);
 
-        $this->twig->addExtension(new TwigFunction() ) ;
+        $this->twigAddExtension();
+        $this->twigAddGlobalVars();
+    }
+
+    protected function setVars($var,$value)
+    {
+        $this->vars[$var] = $value;
+        return $this;
     }
 
     public function render($action,array $vars = [],$reload = true)
@@ -40,60 +55,5 @@ class Action {
 
         $action = str_replace(".","/",$action);
         return  $this->twig->render($action.config('app.layout.extension'), $vars);
-    }
-
-    public function maker($view, $vars = null,$html = false)
-    {
-        $src = $this->validSrc();
-
-        if(file_exists($src.$view)){
-
-            if($html == true)
-            {
-                $view = file_get_contents($src.$view);
-                return $this->replaceTags($vars,$view);
-            }else
-            {
-                $this->addVars($vars);
-                return include_once($src.$view);
-            }
-        }
-        else
-        {
-            throw  new \Exception ('View not found');
-        }
-    }
-
-    private function validSrc()
-    {
-        $public =  config('app.public');
-        $folder =  config('app.layout.folder');
-        ($public == true) ? $src = "../{$folder}" : $src = $folder;
-        return $src;
-    }
-
-    public function addVars($vars)
-    {
-        if(is_array($vars))
-        {
-            foreach ($vars as $key => $var)
-            {
-                $this->view->$key = $var;
-            }
-        }
-    }
-
-    private function replaceTags($vars,$file)
-    {
-        $tag = config('app.layout.tag');
-
-        if(is_array($vars))
-        {
-            foreach ($vars as $key => $var)
-            {
-                $file = str_replace("{$tag[0]}".$key."{$tag[1]}",$var,$file);
-            }
-        }
-        return $file;
     }
 }
