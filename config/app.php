2cef338d4ac884b218174c8f877d68ebe7dd9255
CodeAndCode <angelo.biscola@gmail.com>
2016-06-29 15:23:15 -0300

    Update app.php

--- config/app.php
+++ config/app.php
@@ -1,20 +1,29 @@
 <?php
 
 return [
-        'public'   => true,
-        'timezone' => 'UTC',
+    'public'    => false,
+    'timezone' => 'UTC',
 
-        'database' =>  [ 'host'    => '127.0.0.1',
-                        'dbname'   => 'code',
-                        'username' => 'root',
-                        'password' =>  ''
-        ],
+    'database' => [ 'host'     => '127.0.0.1',
+        'dbname'   => 'hotspot',
+        'username' => 'root',
+        'password' =>  ''
+    ],
 
-        'layout' =>  [ 'folder'    => '',
-                       'default'   => 'app.phtml',
-                       'tag'       => ['{','}'],
-                       'extension' => '.phtml'
-        ]
+    'layout' =>  [ 'folder'     => '../App/views/',
+        'tag'       => ['{','}'],
+        'extension' => '.html.twig',
+        'cache'     => '../../../storage/compilation_cache'
+    ],
+
+    'file'  => [
+        'folder' => 'images'
+    ],
+
+    'cache'  => [ 'active' => true,
+        'folder' => '../../../storage/cache',
+
+    ]
+
 ];
-
 
