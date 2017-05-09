bd5990112144fafdf3b46a1aea86f1a9489dae39
Code&Code <codeandcode.ltda@gmail.com>
2017-05-09 10:58:56 -0300

    create method arrayToObject

--- Helpers/Helper.php
+++ Helpers/Helper.php
@@ -98,3 +98,26 @@ if (! function_exists('viewPath')) {
         return $base.$view.$extension;
     }
 }
+
+if (! function_exists('arrayToObject')) {
+
+    function arrayToObject($array) {
+        if (!is_array($array)) {
+            return $array;
+        }
+        $object = new \stdClass();
+        if (is_array($array) && count($array) > 0) {
+            foreach ($array as $name=>$value) {
+                $name = strtolower(trim($name));
+                if (!empty($name)) {
+                    $object->$name = arrayToObject($value);
+                }
+            }
+            return $object;
+        }
+        else {
+            return FALSE;
+        }
+    }
+}
+
