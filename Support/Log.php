cdad260e8f3bfe43498db6f7bfdd7edd721c0da4
Code&Code <codeandcode.ltda@gmail.com>
2017-07-10 09:56:59 -0300

    Create Class LOG

---
+++ Support/Log.php
@@ -0,0 +1,44 @@
+<?php
+namespace Cac\Support;
+
+class Log
+{
+    private static  $folder;
+    private static  $file;
+
+    private static function init()
+    {
+        self::$folder   = config('app.log.folder');
+        self::$file     = config('app.log.file');
+    }
+
+    private static function getFile()
+    {
+        self::init();
+        return self::$folder ."/".self::$file;
+    }
+
+    public static function logMsg($msg, $level = 'info')
+    {
+        $levelStr = '';
+
+        switch ( $level )
+        {
+            case 'info':
+                $levelStr = 'INFO';
+                break;
+
+            case 'warning':
+                $levelStr = 'WARNING';
+                break;
+
+            case 'error':
+                $levelStr = 'ERROR';
+                break;
+        }
+        $date = date( 'Y-m-d H:i:s' );
+        $msg = sprintf( "[%s] [%s]: %s%s", $date, $levelStr, $msg, PHP_EOL );
+        file_put_contents( self::getFile(), $msg, FILE_APPEND );
+    }
+}
+
