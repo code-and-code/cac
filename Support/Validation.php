af4d84de874c5dacedb9dff53f66857d47387f53
Angelo Biscola <angelo-tec@hotmail.com>
2017-07-10 14:20:17 -0300

    Fix

--- Support/Validation.php
+++ Support/Validation.php
@@ -6,13 +6,24 @@ use Cac\Model\Model;
 
 class Validation
 {
+
     public static function requiredBasic($rules, $attributes)
     {
+        $errors = 0;
+
         foreach ($attributes as $key => $attribute)
         {
             if(in_array($key,$rules))
             {
-                alert('error',"O campo {$key} é obrigatório");
+                if(is_null($attributes[$key]))
+                {
+
+                    if(empty($attribute))
+                    {
+                        alert('error',"o campo {$key} é obrigatório");
+                        $errors++;
+                    }
+                }
             }
         }
         back('Erro de validação','warning');
@@ -20,23 +31,38 @@ class Validation
 
     public static function requireModel($class, $attributes)
     {
+        $errors = 0;
+
         if($class instanceof Model)
         {
             if(property_exists($class,'requested')) {
 
                 foreach ($attributes as $key => $attribute) {
+
                     if (in_array($key, $class->requested)) {
 
-                        alert('error',"o campo {$key} é obrigatório");
+                        if(empty($attribute))
+                        {
+                            alert('error',"o campo {$key} é obrigatório");
+                            $errors++;
+                        }
                     }
                 }
-                throw new ValidationException('Erro de validação');
+
+                if($errors > 0)
+                {
+                    throw new ValidationException('Erro de validação');
+                }
             }
-
-            throw new ValidationException("Compos REQUIRIDOS,não foi configurado no CLASS: ".get_class($class));
-
+            else
+            {
+                throw new ValidationException("Compos REQUIRIDOS,não foi configurado no CLASS: ".get_class($class));
+            }
         }
-        throw new ValidationException(get_class($class)."Não uma ESTANCIA de MODEL");
+        else
+        {
+            throw new ValidationException(get_class($class)."Não uma ESTANCIA de MODEL");
+        }
     }
 }
 
