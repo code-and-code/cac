032b4ef838ce89cccddd6f75e7fbfd28349dc22c
Angelo Biscola <angelo-tec@hotmail.com>
2017-07-11 16:53:48 -0300

    Create Router.php

---
+++ Route/Router.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Cac\Route;
+
+
+class Router {
+
+    private static $routes = array();
+
+    private function __construct() {}
+    private function __clone() {}
+
+    public static function route($pattern) {
+
+        $change = str_replace('$var', '\d+', $pattern['route']);
+        $key    = '/^' . str_replace('/', '\/', $change) . '$/';
+        self::$routes[$key] = arrayToObject($pattern);
+    }
+
+    public static function get($pattern)
+    {
+        $pattern['method'] = 'GET';
+        self::route($pattern);
+
+    }
+
+    public static function post($pattern)
+    {
+        $pattern['method'] = 'POST';
+        self::route($pattern);
+    }
+
+    public static function execute() {
+
+        $url = self::url();
+
+        foreach (self::$routes as $pattern => $config) {
+
+            if (preg_match($pattern, $url->path, $params)) {
+
+                array_shift($params);
+
+                if(isset($config->method))
+                {
+                    if($url->method!= $config->method)
+                    {
+                        return self::notFound("Metodo <b>{$url->path}</b> nao e permitido por essa requicao HTTP. <b>Error 401</b>");
+                    }
+                }
+
+                if(isset($config->auth) && $config->auth)
+                {
+                    if(self::validation() == true)
+                    {
+                        self::runMethod($config->namespace,$config->controller,$config->action,$params);
+                    }
+                    else
+                    {
+                        //$config = config('auth.auth');
+                        //header("Location:".$config['notauthorized']);
+                    }
+                }
+                else
+                {
+                    self::runMethod($config->namespace,$config->controller,$config->action,$params);
+
+                }
+            }else
+            {
+                self::notFound("Pagina <b>{$url->path}</b> nao encontrada. <b> Error 404 </b>");
+            }
+        }
+
+    }
+
+    protected static function url()
+    {
+        $url             = parse_url($_SERVER['REQUEST_URI']);
+        $url['method']   = $_SERVER['REQUEST_METHOD'];
+        $url['notfound'] = true;
+
+        return arrayToObject($url);
+    }
+
+    protected static function validation()
+    {
+        @session_start();
+        if(isset($_SESSION['auth'])){
+            return true;
+        }
+    }
+
+    protected static function notFound($msg)
+    {
+        echo $msg;
+    }
+
+    protected static function runMethod($name,$class,$method,$params)
+    {
+        if (!method_exists(self::getClass($name,$class),$method)){
+
+            self::notFound('Method not found');
+        }
+        else
+        {
+            call_user_func_array([self::getClass($name,$class),$method],$params);
+        }
+    }
+
+    protected static function getClass($name,$class)
+    {
+        $class = $name.ucfirst($class);
+        return new $class;
+    }
+}
