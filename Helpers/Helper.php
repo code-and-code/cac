070ce58fac9ce5e7cd7c2441a9f092700069622f
Code&Code <codeandcode.ltda@gmail.com>
2017-07-14 11:41:20 -0300

    Update Helper.php

--- Helpers/Helper.php
+++ Helpers/Helper.php
@@ -221,3 +221,14 @@ if (! function_exists('redirect')) {
         header("Location:{$url}");
     }
 }
+
+if (! function_exists('dd')) {
+
+    function dd($data) {
+
+        echo '<pre> ';
+        print_r($data);
+        echo '</pre>';
+        die();
+    }
+}
