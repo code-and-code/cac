00a29e8177b838e3ecf362da51f6c26982796b9b
Angelo Biscola <angelo.biscola@gmail.com>
2017-08-04 16:21:27 -0300

    fix

--- src/Model/MysqlConnection.php
+++ src/Model/MysqlConnection.php
@@ -15,25 +15,17 @@ class MysqlConnection
         $this->dbUser = $dbDetails['username'];
         $this->dbPass = $dbDetails['password'];
 
-        $db = "mysql:host=".$this->dbHost.";dbname=".$this->dbName['dbname'].";charset=utf8";
+        $db = "mysql:host=".$this->dbHost.";dbname=".$this->dbName.";charset=utf8";
 
         // Your Code here to connect to database //
         $options = array(
             \PDO::ATTR_PERSISTENT    => true,
             \PDO::ATTR_ERRMODE       => \PDO::ERRMODE_EXCEPTION,
-            //\PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8'
         );
 
         // Create a new PDO instanace
-        try{
-            $this->dbh = new \PDO($db, $this->dbUser['username'], $this->dbPass, $options);
-            return self::$instance;
-        }
-            // Catch any errors
-        catch(\PDOException $e){
+        $this->dbh = new \PDO($db, $this->dbUser, $this->dbPass, $options);
 
-            $e->getMessage();
-        }
     }
 
     public static function connect($dbDetails = array()) {
@@ -43,7 +35,6 @@ class MysqlConnection
             self::$instance = new MysqlConnection($dbDetails);
         }
         return self::$instance;
-
     }
 
     private function __clone() {
